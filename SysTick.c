a6fe5c98a6bebf9306e351b3a6e77c3bd146b43e
slys2k <keithcyr@utexas.edu>
2014-09-18 01:46:31 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Lab3.c

--- SysTick.c
+++ SysTick.c
@@ -36,6 +36,10 @@
 
 #include <stdint.h>
 #include "inc/tm4c123gh6pm.h"
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+void (*PeriodicTask)(void);   // user function
+
 #define NVIC_ST_CTRL_COUNT      0x00010000  // Count flag
 #define NVIC_ST_CTRL_CLK_SRC    0x00000004  // Clock Source
 #define NVIC_ST_CTRL_INTEN      0x00000002  // Interrupt enable
@@ -43,13 +47,29 @@
 #define NVIC_ST_RELOAD_M        0x00FFFFFF  // Counter load value
 
 // Initialize SysTick with busy wait running at bus clock.
-void SysTick_Init(void){
+void SysTick_Init(void(*task)(void)){
   NVIC_ST_CTRL_R = 0;                   // disable SysTick during setup
   NVIC_ST_RELOAD_R = NVIC_ST_RELOAD_M;  // maximum reload value
   NVIC_ST_CURRENT_R = 0;                // any write to current clears it
                                         // enable SysTick with core clock
-  NVIC_ST_CTRL_R = NVIC_ST_CTRL_ENABLE+NVIC_ST_CTRL_CLK_SRC;
+	NVIC_SYS_PRI3_R=(NVIC_SYS_PRI3_R&0x00FFFFFF) | 0X20000000; //PRIORTY 1 
+  NVIC_ST_CTRL_R = NVIC_ST_CTRL_ENABLE+NVIC_ST_CTRL_CLK_SRC+NVIC_ST_CTRL_INTEN;
+	PeriodicTask = task;             // user function
+	EnableInterrupts();	
 }
+
+
+void SysTick_Handler(void){
+	(*PeriodicTask)(); 
+	
+}
+
+
+
+
+
+
+
 // Time delay using busy wait.
 // The delay parameter is in units of the core clock. (units of 20 nsec for 50 MHz clock)
 void SysTick_Wait(uint32_t delay){
